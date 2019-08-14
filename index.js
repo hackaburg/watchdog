d889b041278d33cd864fc089282da0a17890f990
Michael Neu <git@michaeln.eu>
2019-08-14 13:11:48 +0200

    use slack webhook for notifying

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
-#!/usr/bin/env node
-
-const slack = require("@slack/client");
-
-const token = process.env.API_TOKEN;
-const channel = process.env.CHANNEL;
-const icon_url = process.env.ICON_URL;
-
-const client = new slack.WebClient(token);
-
-function notify(message) {
-  return new Promise((resolve, reject) => {
-    client.chat.postMessage(channel, message, {
-      as_user: false,
-      username: "watchdog",
-      icon_url,
-    }, (error, result) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve(result);
-    });
-  });
-}
+const { IncomingWebhook } = require("@slack/webhook");
+const token = process.env.SLACK_WEBHOOK_URL;
+const hook = new IncomingWebhook(token);
 
 for (const signal of ["SIGTERM", "SIGINT", "SIGUSR1", "SIGUSR2"]) {
   process.on(signal, async () => {
     console.log(`received signal ${signal}`);
-    await notify(`received signal ${signal}, about to shut down!`);
+
+    await hook.send({
+      text: `received signal ${signal}, about to shut down!`,
+    });
+
     process.exit();
   });
 }
 
-(async () => {
-  await notify("watchdog started");
-})();
+hook.send({
+  text: "watchdog started",
+});
 
 function wake() {
   setTimeout(wake, 60 * 60 * 1000);
