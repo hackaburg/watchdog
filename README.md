eb7f781165169bbf72e0dcdfbd249b916be27c95
phntxx <meissner.bastian@gmail.com>
2021-03-11 12:28:29 +0100

    Add support for Discord

--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 [![MicroBadger Size](https://img.shields.io/microbadger/image-size/hackaburg/watchdog)](https://hub.docker.com/r/hackaburg/watchdog)
 [![MicroBadger Layers](https://img.shields.io/microbadger/layers/hackaburg/watchdog)](https://hub.docker.com/r/hackaburg/watchdog)
 
-If run inside a docker-compose setup, the watchdog will notify the configured Slack channel about its shutdown.
+If run inside a docker-compose setup, the watchdog will notify the configured Slack channel (or Discord server) about its shutdown.
 
 ## Usage
 
-To get the watchdog, simply pull the `hackaburg/watchdog` image from the Docker hub and pass in your Slack webhook url as an environment variable:
+To get the watchdog, simply pull the `hackaburg/watchdog` image from the Docker hub and pass in your Slack (or Discord) webhook url as an environment variable:
 
 ```bash
 $ docker run \
     --rm \
     -it \
     -e SLACK_WEBHOOK_URL=https://your-slack-webhook_url \
+    -e DISCORD_WEBHOOK_URL=https://your-discord-webhook_url \
     hackaburg/watchdog
 ```
 
--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const { IncomingWebhook } = require("@slack/webhook");
-const token = process.env.SLACK_WEBHOOK_URL;
-const hook = new IncomingWebhook(token);
+const discord = require("discord.js");
+
+const slackToken = process.env.SLACK_WEBHOOK_URL;
+const discordURL = process.env.DISCORD_WEBHOOK_URL;
+
+const slackHook, discordHook;
+
+if (slackToken) {
+  slackHook = new IncomingWebhook(slackToken);
+}
+
+if (discordURL) {
+  // Structure of discord webhook URL: https://discord.com/api/webhooks/[id]/[token]
+  const discordCredentials = discordURL
+    .replace("https://discord.com/api/webhooks/", "")
+    .split("/");
+
+  discordHook = new discord.WebhookClient(
+    discordCredentials[0],
+    discordCredentials[1]
+  );
+}
 
 for (const signal of ["SIGTERM", "SIGINT", "SIGUSR1", "SIGUSR2"]) {
   process.on(signal, async () => {
     console.log(`received signal ${signal}`);
 
-    await hook.send({
-      text: `received signal ${signal}, about to shut down!`,
-    });
+    if (slackToken) {
+      await slackHook.send({
+        text: `received signal ${signal}, about to shut down!`,
+      });
+    }
+
+    if (discordID && discordToken) {
+      discordHook.send(`received signal ${signal}, about to shut down!`);
+    }
 
     process.exit();
   });
 }
 
-hook.send({
-  text: "watchdog started",
-});
+if (slackToken) {
+  slackHook.send({
+    text: "watchdog started",
+  });
+}
+
+if (discordID && discordToken) {
+  discordHook.send("watchdog started");
+}
 
 function wake() {
   setTimeout(wake, 60 * 60 * 1000);
